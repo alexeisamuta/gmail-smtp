df6fffe77fb55ee193adbcf5687fc0430df46d06
alexeisamuta <alexeisamuta@gmail.com>
2020-11-18 22:51:48 +0300

    13 test sending a message

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const smtp_password = process.env.SMTP_PASSWORD || '---'
 
 app.use(cors())
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 
