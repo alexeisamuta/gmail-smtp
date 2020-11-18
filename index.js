b9046e419577c7eaaf026bed1a4261bcebb71343
alexeisamuta <alexeisamuta@gmail.com>
2020-11-18 23:21:31 +0300

    18 test sending a message

--- index.js
+++ index.js
@@ -6,32 +6,10 @@ const app = express()
 
 const port = process.env.PORT || 3010
 
-const smtp_login = process.env.SMTP_LOGIN || "alexeisamuta@gmail.com"
-const smtp_password = process.env.SMTP_PASSWORD || "725z79z32z"
-
-
+const smtp_login = process.env.SMTP_LOGIN || "---"
+const smtp_password = process.env.SMTP_PASSWORD || "---"
 
 app.use(cors())
-
-app.use(function (req, res, next) {
-    var origins = [
-        'http://localhost:3000/',
-        'https://alexeisamuta.github.io/',
-        'https://smtp-server-for-portfolio-node.herokuapp.com/'
-    ];
-
-    for(var i = 0; i < origins.length; i++){
-        var origin = origins[i];
-
-        if(req.headers.origin.indexOf(origin) > -1){
-            res.header('Access-Control-Allow-Origin', req.headers.origin);
-        }
-    }
-
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
