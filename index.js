c6e71a045b650fbfe28c7ee8f89c48be1c72ecd3
alexeisamuta <alexeisamuta@gmail.com>
2020-11-19 15:04:06 +0300

    21 test sending a message

--- index.js
+++ index.js
@@ -24,16 +24,16 @@ const transporter = nodemailer.createTransport({
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.post('/sendMessage', async (req, res) => {
-
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Max-Age", "1800");
     res.setHeader("Access-Control-Allow-Headers", "content-type");
     res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
+    res.send('Hello World!')
+})
+
+app.post('/sendMessage', async (req, res) => {
+
 
     let {name, email, message} = req.body
 
